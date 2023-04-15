bc4c9b49b7df076288cf4d152a9f90fb145da363
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-04-15 22:53:01 +0300

    Подключение экранов к роутеру

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,12 @@
   </v-list-item>
   <v-divider></v-divider>
   <v-list dense>
-  <v-list-item
-  v-for="link in links"
-  :key="link.title"
-  >
-  <template v-slot:prepend>
-    <v-icon :icon="link.icon"></v-icon>
+    <v-list-item
+v-for="link in links"
+:key="link.title" :to="link.url"
+>
+<template v-slot:prepend>
+<v-icon :icon="link.icon"></v-icon>
 </template>
 <v-list-item-title>{{ link.title }}</v-list-item-title>
 </v-list-item>
@@ -28,10 +28,9 @@
 <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 <v-spacer></v-spacer>
 <v-toolbar-items class="hidden-sm-and-down">
-<v-btn
-text
+  <v-btn
 v-for="link in links"
-:key="link.title"
+:key="link.title" :to="link.url"
 >
 <v-icon
 start
