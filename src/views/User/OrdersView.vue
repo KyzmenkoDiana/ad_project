3f78e2ff087ed336ea017078718535fcbf6d993c
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2023-04-17 13:14:01 +0300

    Подготовка и подключение данных для экрана заказов

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -7,34 +7,41 @@
     </v-row>
     </v-container>
     <v-list subheader two-line flat>
-<v-list-item>
-
+<v-list-item v-for="order in orders" 
+	:key="order.id">
 <template v-slot:prepend>
 <v-list-item-action>
 <v-checkbox
-input-value="false"
+:input-value="order.done"
 color="primary"
 ></v-checkbox>
 </v-list-item-action>
 </template>
-
-<v-list-item-title>Title</v-list-item-title>
-<v-list-item-subtitle>Phone</v-list-item-subtitle>
+<v-list-item-title>{{ order.name }}</v-list-item-title>
+<v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
 
 <template v-slot:append>
 <v-list-item-action>
-<v-btn class="primary">Open</v-btn>
+<v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
 </v-list-item-action>
 </template>
 </v-list-item>
 </v-list>
-
 </template>
     
     <script>
     export default {
     data () {
     return {
+        orders: [
+{
+		id:"123",
+		name:"Kostya",
+		phone:"+7(978)000-00-05",
+		adId:"1",
+		done:true
+	}
+	]
     }
     }
     }
